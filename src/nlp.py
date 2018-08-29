ed9239ec0b8d75f421c33260284f7dce90d5614b
lucasgoncdasilva <30935484+lucasgoncdasilva@users.noreply.github.com>
2018-08-29 06:47:22 -0300

    Add files via upload

--- src/nlp.py
+++ src/nlp.py
@@ -1,18 +1,22 @@
 def sgb_most_common_words(corpus,
-                          add_stop_words=[],
-                          tema='' ,
+                          add_stop_words=[""],
+                          bloco='' ,
                           palestrante='',
-                          arquivo='Most_Common_Words.csv'):
+                          arquivo='Most_Common_Words.csv',
+                          save_csv=False
+                          ):
+
     """
-        Função que recebe um texto, recebe identificadores (tema, palestrante...) e
-        exporta um csv com contagem das palavras para o tema/palestrante.
+        Função que recebe um texto, recebe identificadores (bloco, palestrante...) e
+        exporta um csv com contagem das palavras para o bloco/palestrante.
 
         Parâmetros:
 
             str     corpus:         Texto a ter as palavras contadas
             list    add_stop_words: (opcional) Lista dedicada a palavras que a biblioteca nltk não remove sozinha.
-            str     tema:           (opcional) Tema do texto
+            str     bloco:           (opcional) bloco do festival
             str     palestrante:    (opcional) Palestrante
+            bool    save_csv:       (opcional) 
             str     arquivo:        (padrão: 'Most_Common_Words.csv') Nome do Arquivo a ser lido/salvo.
 
     """
@@ -27,22 +31,38 @@ def sgb_most_common_words(corpus,
 
     stopWords_nltk = set(stopwords.words('portuguese'))
 
-    stopWords = stopWords_nltk.union(set(add_stop_words))
+    chumbadas = set(["apenas","outro","pode","trás","lado","frente"])
 
-    no_stops = [w for w in tokens if w not in stopWords]
+    stopWords = stopWords_nltk.union(set(add_stop_words)).union(chumbadas)
 
-    bag_of_words = Counter(no_stops)
+    no_stops = [w for w in tokens if w not in stopWords and len(w) > 2]
 
-    Most_Common_Words = bag_of_words.most_common()
+    if save_csv:    
+        if not os.path.isfile(arquivo):
+            with open(arquivo, 'w') as csvfile:
+                Writer = csv.writer(csvfile)
+                Writer.writerow(['palavra','bloco','palestrante'])
+        for word in no_stops:
+            with open(arquivo,'a') as csvfile:
+                Writer = csv.writer(csvfile)
+                Writer.writerow([word,bloco,palestrante])
 
-    if not os.path.isfile(arquivo):
-        with open(arquivo, 'w') as csvfile:
-            Writer = csv.writer(csvfile)
-            Writer.writerow(['palavra','contagem','tema','palestrante'])
+    rows = [[w,bloco,palestrante] for w in no_stops]
+    return rows
 
-    for word_count in Most_Common_Words:
-        with open(arquivo,'a') as csvfile:
-            Writer = csv.writer(csvfile)
-            Writer.writerow([word_count[0],word_count[1],tema,palestrante])
 
-    return Most_Common_Words
+if __name__ == '__main__':
+
+    teste = """ Teste: 3 pratos de trigo para 3 tristes tigres."""
+
+    from sgb_sheets_api import update_spreadsheet
+
+    rows = sgb_most_common_words(teste, palestrante="Ditado Popular", bloco="Trava línguas")
+
+    update_spreadsheet( rows,
+                        headers = [['palavra','bloco','palestrante']],
+                        spreadsheetId='12nX-xkjk5YiZvNhf0SXHDusefO942CRpxlgkDJeD5qg',
+                        sheet='Tabela1'
+                    )
+
+
---
+++ src/sgb_sheets_api.py
@@ -0,0 +1,121 @@
+ #!/usr/bin/env python3 
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import httplib2
+import os
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from glob import glob
+
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets' 
+
+CLIENT_SECRET_FILE = 'client_secret.json' # Available at: https://console.developers.google.com/apis/credentials?project=gestaodeleadsteste&hl=pt-br
+APPLICATION_NAME = 'sgb-cloud-teste' 
+
+def get_credentials(): # Função importada do site do google sheets API
+    """Gets valid user credentials from storage.
+    
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                    'sheets.googleapis.com-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+def update_spreadsheet( rows, headers,
+                        spreadsheetId, sheet ):
+    """
+    Function for appending a new row in a Google Spreadsheet.
+
+    list of lists   rows 
+    list of lists   headers
+    str             spreadsheetId
+    list of lists   headers (Ex. [["Header 1", "Header 2","Header 3"]])
+    str             sheet (Ex. "Sheet1")
+    
+    """
+
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?'
+                    'version=v4')
+    service = discovery.build('sheets', 'v4', http=http,
+                                discoveryServiceUrl=discoveryUrl)
+    
+    global link 
+
+    link = 'https://docs.google.com/spreadsheets/d/'+ spreadsheetId +'/edit#gid=0'
+
+    rangeName = sheet + '!A1:C'
+
+    result = service.spreadsheets().values().get(
+                                    spreadsheetId=spreadsheetId,
+                                    range=rangeName).execute()
+    current_values = result.get('values', [])
+
+    last_row = len(current_values)
+
+    if last_row == 0:
+        body = {
+            'values': headers 
+            }
+
+        rangeName = sheet + '!A1:C1'
+
+        result = service.spreadsheets().values().update(
+        spreadsheetId=spreadsheetId, range=rangeName,
+        valueInputOption='USER_ENTERED', body=body).execute()
+
+        last_row=1
+
+    rangeName = sheet + '!A' + str(last_row+1) + ':C'
+
+    body = {
+            'values': rows 
+            }
+        
+    
+
+    
+    result = service.spreadsheets().values().update(
+            spreadsheetId=spreadsheetId, range=rangeName,
+            valueInputOption='USER_ENTERED', body=body).execute()
+
+if __name__ == '__main__':
+    update_spreadsheet( [['Teste','Teste','Teste']],
+                        headers = [['palavras','bloco','palestrante']],
+                        spreadsheetId='12nX-xkjk5YiZvNhf0SXHDusefO942CRpxlgkDJeD5qg',
+                        sheet='Tabela1'
+                        )
+
+    print('\n\nGoogle Spreadsheet updated: \n'+ link + '\n')
+    
+    
+
